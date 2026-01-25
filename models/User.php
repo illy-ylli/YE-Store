d7bf34a6d6b0e848ce97fb168925e35537ddeaae
erionbajraktari06 <erionbajraktari06@gmail.com>
2026-01-25 02:02:23 +0100

    Rregullimi i user.php dhe rishkrimi i saj per ruajtje te infos te userit

--- models/User.php
+++ models/User.php
@@ -1,51 +1,24 @@
 <?php
+require_once 'config/Database.php';
+
 class User {
     private $conn;
-    private $table = "users";
-
-    public $id;
-    public $username;
-    public $email;
-    public $password;
-    public $full_name;
-    public $profile_picture;
-    public $country;
-    public $role;
-    public $created_at;
-
-    public function __construct($db) {
-        $this->conn = $db;
+    
+    public function __construct() {
+        $this->conn = Database::getConnection();
     }
-
-    // regjistrimi i userit te ri
-    public function register() {
-        $query = "INSERT INTO " . $this->table . " 
-                  SET username=:username, email=:email, password=:password, 
-                      full_name=:full_name, country=:country";
+    
+    public function login($username, $password) {
+        $sql = "SELECT * FROM users WHERE username = :username OR email = :username";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([':username' => $username]);
         
-        $stmt = $this->conn->prepare($query);
-
-              // mshefe passwordin me hashtag
-        $hashed_password = password_hash($this->password, PASSWORD_DEFAULT);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
         
-        $stmt->bindParam(":username", $this->username);
-        $stmt->bindParam(":email", $this->email);
-        $stmt->bindParam(":password", $hashed_password);
-        $stmt->bindParam(":full_name", $this->full_name);
-        $stmt->bindParam(":country", $this->country);
-        
-        return $stmt->execute();
+        if ($user && password_verify($password, $user['password'])) {
+            return $user;
+        }
+        return false;
     }
-
-      // login useri
-    public function login() {
-        $query = "SELECT id, username, email, password, role, full_name 
-                  FROM " . $this->table . " 
-                  WHERE username = :username LIMIT 1";
-        
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(":username", $this->username);
-        $stmt->execute();
-        
-        if($stmt->rowCount() > 0) {
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+  

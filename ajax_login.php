cc956f91040537a75efb0adeeb87482ce87cbcfb
erionbajraktari06 <erionbajraktari06@gmail.com>
2026-01-31 23:11:15 +0100

    infot e userit (email, username, password) per testim

--- ajax_login.php
+++ ajax_login.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $db = new Database();
     $conn = $db->getConnection();
     
-    // per testim te adminit me password te bere vet
+    // per testim te adminit me password te koduar
     if ($username === 'admin' && $password === 'admin123') {
         $_SESSION['user_id'] = 1;
         $_SESSION['username'] = 'admin';
@@ -25,4 +25,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit;
     }
     
+     // per testim te userit me password te koduar
+    if ($username === 'filon' && $password === 'user123') {
+        $_SESSION['user_id'] = 2;
+        $_SESSION['username'] = 'filon';
+        $_SESSION['role'] = 'user';
+        $_SESSION['email'] = 'filoni67@example.com';
+        
+        echo json_encode([
+            'success' => true,
+            'role' => 'user'
+        ]);
+        exit;
+    }
    
